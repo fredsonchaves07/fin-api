10c2ad742f5b1ae7f7f5ce6593cd62742e62bd5d
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-09 13:05:49 -0300

    feat: implementation middeware validation account

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ app.use(express.json())
 
 const customers = []
 
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers
+
+    const customer = customers.find(
+        customer => customer.cpf === cpf
+    )
+
+    if(!customer){
+        return response.status(400).json({
+            'error': 'Customer not found' 
+        })
+    }
+
+    request.customer = customer
+
+    return next()
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body
 
@@ -29,5 +47,13 @@ app.post('/account', (request, response) => {
 
     return response.status(201).send()
 })
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+
+    return response.json(customer.statement)
+})
+
+
 
 app.listen(5000)
