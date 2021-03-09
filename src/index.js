4592b917c953d481cc5882cb2774afc9cd10fc72
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-09 12:21:56 -0300

    feat: create account implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const express = require('express')
+const { v4: uuidv4 } = require("uuid")
 
 const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body
+
+    const customAlteadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    )
+
+    if(customAlteadyExists){
+        return response.status(400).json({
+            error: 'Customer already Exists'
+        })
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send()
+})
 
 app.listen(5000)
